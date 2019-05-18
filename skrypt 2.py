e92d115eb3566bc2dd263754de4a7a24853227c2
ziajek444 <ziajkowski.marcin@gmail.com>
2019-05-18 18:43:41 +0200

    new changes

---
+++ skrypt 2.py
@@ -0,0 +1,27 @@
+# 1 Edycja zmiennych tekstowych
+
+zmienna01 = 50
+zmienna02 = 'A'
+zmienna03 = chr(zmienna01)
+zmienna04 = ord(zmienna02)
+finish = " w tabeli ASCII\n"
+print(str(zmienna01)+": "+zmienna03,zmienna02+": "+str(zmienna04),sep=finish,end=finish)
+
+zmienna05 = 'któtki tekst między apostrofami'
+zmienna06 = "któtki tekst między cudzysłowami"
+zmienna07 = '''Bardzo długi tekst,
+w którym dozwolone są znaki końca linii,
+bez błędu kompilacji.
+'''
+
+print(zmienna05,zmienna06,zmienna07,sep="\n------------------------------\n")
+
+# ----------------------------
+
+
+
+
+
+
+
+
